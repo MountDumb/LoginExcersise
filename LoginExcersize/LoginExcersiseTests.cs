7741921ce63a82a893778445f4c798ef2d312752
UrbanSkurk <UrbanSkurk@VARMEVRK>
2016-09-19 11:39:17 +0200

    Add project files.

---
+++ LoginExcersize/LoginExcersiseTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LoginExcersize
+{
+    [TestClass]
+    public class LoginExcersiseTests
+    {
+        [TestMethod]
+        public void CreateUser()
+        {
+        }
+    }
+}
---
+++ LoginExcersize/User.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace LoginExcersize
+{
+    class User : IUser
+    {
+        //Fields
+        private string username;
+        private string password;
+        
+
+            public User()
+        {
+
+        }
+
+        //Methods
+
+            public string Hasher(string s)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(s);
+            SHA256 hash = SHA256Managed.Create();
+
+            byte[] hashedstring = hash.ComputeHash(bytes);
+            return hashedstring.ToString();
+            
+        }
+
+        //Properties
+        public string UserName
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        
+        public string Password
+        {
+            get { return password; }
+            set { password = Hasher(value); }
+        }
+      
+        
+        
+    }
+}
